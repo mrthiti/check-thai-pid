4fc567056a9bf83ceb1508cd1c05e91faafd901a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-11-28 09:53:51 +0700

    refactor

--- src/check-thai-pid.js
+++ src/check-thai-pid.js
@@ -1,12 +1,10 @@
 module.exports = pid => {
   if (!pid) return false
-
-  const PID = pid.toString()
-
-  if (PID.length !== 13) return false
+  const isNot13Digits = x => ((Math.log(x) * Math.LOG10E + 1) | 0) !== 13
+  if (isNot13Digits(pid)) return false
 
   let sum = 0
-  for (let i = 0; i < 12; i++) sum += parseFloat(PID.charAt(i)) * (13 - i)
+  for (let i = 0; i < 12; i++) sum += parseFloat(pid.charAt(i)) * (13 - i)
 
-  return (11 - sum % 11) % 10 === parseFloat(PID.charAt(12))
+  return (11 - sum % 11) % 10 === parseFloat(pid.charAt(12))
 }
