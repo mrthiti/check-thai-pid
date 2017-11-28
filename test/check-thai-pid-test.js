f55969bfed4ade9f406558579214ee7e646f03ab
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-11-28 09:16:15 +0700

    refactor

--- test/check-thai-pid-test.js
+++ test/check-thai-pid-test.js
@@ -5,6 +5,7 @@ describe('CheckThaiPID', () => {
   it('PID is correct(input number)', () => {
     expect(checkThaiPID(3731052662747)).to.equal(true)
   })
+
   it('PID is correct(input string)', () => {
     expect(checkThaiPID('3731052662747')).to.equal(true)
   })
@@ -12,18 +13,23 @@ describe('CheckThaiPID', () => {
   it('PID is not correct(input number not 13 digit)', () => {
     expect(checkThaiPID(373105266274)).to.equal(false)
   })
+
   it('PID is not correct(input number incorrect format)', () => {
     expect(checkThaiPID(1111111111111)).to.equal(false)
   })
+
   it('PID is not correct(input string not 13 digit)', () => {
     expect(checkThaiPID('373105266274')).to.equal(false)
   })
+
   it('PID is not correct(input string incorrect format)', () => {
     expect(checkThaiPID('1111111111111')).to.equal(false)
   })
+
   it('PID is not correct(input string not number)', () => {
     expect(checkThaiPID('test')).to.equal(false)
   })
+
   it('PID is not correct(input is null)', () => {
     expect(checkThaiPID(null)).to.equal(false)
     expect(checkThaiPID()).to.equal(false)
