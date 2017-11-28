40de5be7bb8fc55a92c85a918db4c63dcdaacbe8
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-11-28 09:13:50 +0700

    refactor

--- src/check-thai-pid.js
+++ src/check-thai-pid.js
@@ -1,14 +1,12 @@
 module.exports = personalID => {
+  if (personalID == null) return false
 
-    if (personalID == null) return false
+  const PID = personalID.toString()
 
-    const PID = personalID.toString()
+  if (PID.length !== 13) return false
 
-    if (PID.length != 13) return false
+  let sum = 0
+  for (let i = 0; i < 12; i++) sum += parseFloat(PID.charAt(i)) * (13 - i)
 
-    let sum = 0
-    for (let i = 0; i < 12; i++)
-        sum += parseFloat(PID.charAt(i)) * (13 - i)
-
-    return ((11 - sum % 11) % 10 == parseFloat(PID.charAt(12)))
+  return (11 - sum % 11) % 10 === parseFloat(PID.charAt(12))
 }
